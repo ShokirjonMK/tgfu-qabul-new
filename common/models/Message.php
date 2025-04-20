e4d50ff9343ea9fee4b8eb5f34b7954d054de6c9
ShokirjonMK <mkshokirjon@gmail.com>
2025-04-20 10:51:58 +0500

    Update Message.php

--- common/models/Message.php
+++ common/models/Message.php
@@ -25,80 +25,48 @@ class Message extends \yii\db\ActiveRecord
     public static function sendSms($phone, $text)
     {
         $phone = preg_replace("/[^0-9]/", "", $phone);
-        $email = 'tzubukhara@gmail.com';
-        $password = 'A1wgNalEGTefGmgckgZVgMchP3C1bXh6qwZ6Juv8';
-        $url = 'http://notify.eskiz.uz/api/auth/login';
-        $client = new Client();
-        $response = $client->createRequest()
-            ->setFormat(Client::FORMAT_JSON)
-            ->setMethod("POST")
-            ->setUrl($url)
-            ->setData([
-                'email' => $email,
-                'password' => $password
-            ])
-            ->send();
-        $data = (json_decode($response->content))->data;
-        $token = $data->token;
-        $from = "4546";
-        $url = 'http://notify.eskiz.uz/api/message/sms/send';
-        $textNew = "ZARMED UNIVERSITETI qabul saytiga ro‘yxatdan o‘tish uchun tasdiqlash kodi: " . $text;
-        $response = $client->createRequest()
-            ->setFormat(Client::FORMAT_JSON)
-            ->setMethod("POST")
-            ->setUrl($url)
-            ->addHeaders(['Authorization' => 'Bearer ' . $token])
-            ->setData([
-                'message' => $textNew,
-                'mobile_phone' => $phone,
-                'from' => $from
-            ])
-            ->send();
-
-        return ($response->statusCode);
-
-//        $phone = preg_replace("/[^0-9]/", "", $phone);
-//        $text = 'Tasdiqlash kodi:'. $text;
-//        $data = '{
-//                "messages":
-//                    [
-//                        {
-//                        "recipient":'.$phone.',
-//                        "message-id":"abc000000001",
-//                            "sms":{
-//                                "originator": "3700",
-//                                "content": {
-//                                    "text": "'.$text.'"
-//                                }
-//                            }
-//                        }
-//                    ]
-//                }';
-//        $curl = curl_init();
-//        curl_setopt_array($curl, array(
-//            CURLOPT_URL => "https://send.smsxabar.uz/broker-api/send",
-//            CURLOPT_RETURNTRANSFER => true,
-//            CURLOPT_ENCODING => "",
-//            CURLOPT_MAXREDIRS => 10,
-//            CURLOPT_TIMEOUT => 30,
-//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//            CURLOPT_CUSTOMREQUEST => "POST",
-//            CURLOPT_POSTFIELDS => $data,
-//            CURLOPT_HTTPHEADER => array(
-//                "Authorization: Basic ".base64_encode("tgfu:M40KH237PwM]"),
-//                "cache-control: no-cache",
-//                "content-type: application/json",
-//            ),
-//        ));
-//        $response = curl_exec($curl);
-//        $err = curl_error($curl);
-//        curl_close($curl);
-//        return $phone." --- ".$response;
+        $text = 'Toshkent Gumanitar Fanlar Universiteti qabul tizimi  - tasdiqlash kodi: '. $text;
+        $data = '{
+                "messages":
+                    [
+                        {
+                        "recipient":'.$phone.',
+                        "message-id":"abc000000001",
+                            "sms":{
+                                "originator": "3700",
+                                "content": {
+                                    "text": "'.$text.'"
+                                }
+                            }
+                        }
+                    ]
+                }';
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://send.smsxabar.uz/broker-api/send",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Basic ".base64_encode("tgfu:6Ya!W5n=Xk9c"),
+                "cache-control: no-cache",
+                "content-type: application/json",
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        return $phone." --- ".$response;
     }
+
+
 
     public static function sendedSms($phone, $text)
     {
-        return true;
         $phone = preg_replace("/[^0-9]/", "", $phone);
         $data = '{
                 "messages":
@@ -126,7 +94,7 @@ class Message extends \yii\db\ActiveRecord
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => $data,
             CURLOPT_HTTPHEADER => array(
-                "Authorization: Basic ".base64_encode("tgfu:M40KH237PwM]"),
+                "Authorization: Basic ".base64_encode("tgfu:6Ya!W5n=Xk9c"),
                 "cache-control: no-cache",
                 "content-type: application/json",
             ),
@@ -134,7 +102,9 @@ class Message extends \yii\db\ActiveRecord
         $response = curl_exec($curl);
         $err = curl_error($curl);
         curl_close($curl);
-        return $response;
+        return $phone." --- ".$response;
     }
+
+
 }
 
