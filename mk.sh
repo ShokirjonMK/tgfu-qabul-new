8b90bd3bdc73f5a39c41b5956064774159ed9950
ShokirjonMK <mkshokirjon@gmail.com>
2025-04-26 13:04:00 +0000

    ssss

--- mk.sh
+++ mk.sh
@@ -1,3 +1,5 @@
+REPO_DIR_API="/home/faceid/apps/tgfu-qabul-new"
+BACKUP_DIR="/home/backup/tgfuqabul_new"
 #!/bin/bash
 
 echo "[INFO] MySQL zaxira jarayoni boshlanyapti..."
@@ -5,7 +7,7 @@ echo "[INFO] MySQL zaxira jarayoni boshlanyapti..."
 now=$(date +%Y-%m-%d_%H-%M-%S)
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-REPO_DIR_API="/home/dev-1/apps/tgfu-qabul-new"
+#REPO_DIR_API="/home/dev-1/apps/tgfu-qabul-new"
 ENV_FILE="$REPO_DIR_API/.env"
 
 # === .env yuklash ===
@@ -22,7 +24,7 @@ DB_NAME=${DOCKER_PROJECT_NAME}
 MYSQL_PASSWORD=${DATABASE_PASSWORD}
 DOCKERFILE="$REPO_DIR_API/docker-compose.yml"
 
-BACKUP_DIR="/home/backup/tgfu"
+#BACKUP_DIR="/home/backup/tgfu"
 SQL_FILE="$BACKUP_DIR/$PROJECT_NAME-$now.sql"
 ARCHIVE_FILE="$BACKUP_DIR/$PROJECT_NAME-$now.tar.gz"
 
