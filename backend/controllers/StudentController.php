dbf6e9dac80cd9a7d3689eacdb83659ad8f413e8
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-29 14:04:58 +0500

    Update StudentController.php

--- backend/controllers/StudentController.php
+++ backend/controllers/StudentController.php
@@ -177,7 +177,7 @@ class StudentController extends Controller
     public function actionView($id)
     {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+            'model' => $this->findModelView($id),
         ]);
     }
 
@@ -730,7 +730,22 @@ class StudentController extends Controller
     protected function findModel($id)
     {
         if (($model = Student::findOne(['id' => $id])) !== null) {
-            return $model;
+            $user = $model->student->user;
+            if ($user->status == 10) {
+                return $model;
+            }
+        }
+
+        throw new NotFoundHttpException(\Yii::t('app', 'The requested page does not exist.'));
+    }
+
+    protected function findModelView($id)
+    {
+        if (($model = Student::findOne(['id' => $id])) !== null) {
+            $user = $model->student->user;
+            if ($user->status == 10) {
+                return $model;
+            }
         }
 
         throw new NotFoundHttpException(\Yii::t('app', 'The requested page does not exist.'));
